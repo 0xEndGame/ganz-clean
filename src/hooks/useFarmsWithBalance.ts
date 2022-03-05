ad84739152ee56f576349c7032f74a65fc6bffed
Your Name <you@example.com>
2022-03-05 22:19:56 +0100

    44

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingCoal',
+        name: 'pendingWaifu',
         params: [farm.pid, account],
       }))
 
