6e764c7eb3b2f8fd5b7ff1d109aee56c38b8441a
Your Name <you@example.com>
2022-03-05 16:41:41 +0100

    444

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -25,7 +25,7 @@ const Hero = styled.div`
   ${({ theme }) => theme.mediaQueries.lg} {
     background-image: url('/images/egg/3b.png');
     background-position: right center;
-    height: auto;
+    height: 144px;
     max-width: auto;
     padding-top: 0;
   }
