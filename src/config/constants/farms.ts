b9779dbd8ae2a3a0e801635514d551298407df4d
Merkel <you@example.com>
2022-03-08 22:59:19 +0100

    55

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -53,14 +53,79 @@ const farms: FarmConfig[] = [
   {
     pid: 3,
     risk: 3,
-    lpSymbol: 'WFTM-USDT',
+    lpSymbol: 'WFTM-DAI',
     lpAddresses: {
-      250: '0x5965e53aa80a0bcf1cd6dbdd72e6a9b2aa047410',
+      250: '0xe120ffBDA0d14f3Bb6d6053E90E63c572A66a428',
       97: '',
     },
-    tokenSymbol: 'WFTM',
+    tokenSymbol: 'DAI',
     tokenAddresses: {
-      250: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
+      250: '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
+      97: '',
+    },
+    quoteTokenSymbol: QuoteToken.WFTM,
+    quoteTokenAdresses: contracts.wftm,
+  },
+  {
+    pid: 4,
+    risk: 2,
+    lpSymbol: 'BOO-WFTM',
+    lpAddresses: {
+      250: '0xEc7178F4C41f346b2721907F5cF7628E388A7a58',
+      97: '',
+    },
+    tokenSymbol: 'BOO',
+    tokenAddresses: {
+      250: '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE',
+      97: '',
+    },
+    quoteTokenSymbol: QuoteToken.WFTM,
+    quoteTokenAdresses: contracts.wftm,
+  },
+  {
+    pid: 5,
+    risk: 1,
+    lpSymbol: 'WETH-WFTM',
+    lpAddresses: {
+      250: '0xf0702249f4d3a25cd3ded7859a165693685ab577',
+      97: '',
+    },
+    tokenSymbol: 'WETH',
+    tokenAddresses: {
+      250: '0x74b23882a30290451A17c44f4F05243b6b58C76d',
+      97: '',
+    },
+    quoteTokenSymbol: QuoteToken.WFTM,
+    quoteTokenAdresses: contracts.wftm,
+  },
+  {
+    pid: 6,
+    risk: 4,
+    lpSymbol: 'WBTC-WFTM',
+    lpAddresses: {
+      250: '0xFdb9Ab8B9513Ad9E419Cf19530feE49d412C3Ee3',
+      97: '',
+    },
+    tokenSymbol: 'WBTC',
+    tokenAddresses: {
+      250: '0x321162Cd933E2Be498Cd2267a90534A804051b11',
+      97: '',
+    },
+    quoteTokenSymbol: QuoteToken.WFTM,
+    quoteTokenAdresses: contracts.wftm,
+  },
+  {
+    pid: 7,
+    risk: 5,
+    isTokenOnly: true,
+    lpSymbol: 'WAIFU',
+    lpAddresses: {
+      250: '0xE80792d7c9C39fc8456ECE4294E990371756c4dC',
+      97: '', // COAL-USDT LP
+    },
+    tokenSymbol: 'COAL',
+    tokenAddresses: {
+      250: '0xDBCB8249a5CF2B2CA9C241F76B14E44B7e5AeaD8',
       97: '',
     },
     quoteTokenSymbol: QuoteToken.USDT,
