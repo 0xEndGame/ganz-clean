cc12eb14f96fe8ffcad2299a82bd170d6c7e7455
Your Name <you@example.com>
2022-12-26 18:36:24 +0100

    545

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingWaifu',
+        name: 'pendingBanhmi',
         params: [farm.pid, account],
       }))
 
--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -63,7 +63,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingWaifu',
+      name: 'pendingBanhmi',
       params: [farm.pid, account],
     }
   })
