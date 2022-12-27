40cd85477d47ae90c1b8f7dcd18161ba2fecdc75
Your Name <you@example.com>
2022-12-27 06:52:44 +0100

    222

--- src/views/Home/components/NotificationCard.tsx
+++ src/views/Home/components/NotificationCard.tsx
@@ -9,83 +9,39 @@ import { useState } from 'react';
 
 
 const StyledNotificationCard = styled(Card)`
-.popup {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: none;
-  }
-  
-  .popup.open {
-    display: block;
-  }
-  
-  .popup-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 300px;
-    height: 200px;
-    background-color: white;
-    border: 1px solid black;
-    padding: 10px;
-  }
-  
-  .close-button {
-    position: absolute;
-    top: 5px;
-    right: 5px;
-    font-size: 24px;
-    font-weight: bold;
-    border: none;
-    background-color: transparent;
-    color: black;
-    cursor: pointer;
-  }
+.notification-box {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 300px;
+  height: 50px;
+  background-color: white;
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.notification-text {
+  float: left;
+  width: calc(100% - 50px);
+  height: 100%;
 `
 
-  
-  // React component for the trigger button
-  function TriggerButton() {
-    const [isOpen, setIsOpen] = useState(false);
-  
-  }
 
 
+function NotificationBox() {
+  const [isOpen, setIsOpen] = useState(true);
 
-  const NotificationCard = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const TranslateString = useI18n()
-    // const data = useGetStats()
-    const totalValue = useTotalValue();
-    // const tvl = totalValue.toFixed(2);
-  
-    return (
-        <StyledNotificationCard>
-        <div className={`popup ${isOpen ? 'open' : 'closed'}`}>
-        <div className="popup-content">
-          <div className="popup-text">Hello, world!</div>
-          <button type="button"
-            className="close-button"
-            onClick={() => setIsOpen(false)}
-          >
-            X
-          </button>
-        </div>
-      </div>
-
-      <button type="button"
-        className="trigger-button"
-        onClick={() => setIsOpen(true)}
+  return (
+    <div className={`notification-box ${isOpen ? 'open' : 'closed'}`}>
+      <div className="notification-text">Hello, world!</div>
+      <button
+        className="close-button"
+        onClick={() => setIsOpen(false)}
       >
-        Show Pop-Up
+        X
       </button>
-      </StyledNotificationCard>
-    )
-  }
+    </div>
+  );
+}
   
   export default NotificationCard
