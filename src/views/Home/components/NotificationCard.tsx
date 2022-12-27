ef87a7d2e943e407c8a4aa26db995d12e62fea26
Your Name <you@example.com>
2022-12-27 07:02:19 +0100

    222

--- src/views/Home/components/NotificationCard.tsx
+++ src/views/Home/components/NotificationCard.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 import { Card, CardBody, Heading, Skeleton, Text } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
