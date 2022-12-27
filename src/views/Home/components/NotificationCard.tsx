3719b481370a5f236a8fe359af270665be9cbeb5
Your Name <you@example.com>
2022-12-27 06:58:26 +0100

    44

--- src/views/Home/components/NotificationCard.tsx
+++ src/views/Home/components/NotificationCard.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import styled from 'styled-components'
 import { Card, CardBody, Heading, Skeleton, Text } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import { useGetStats } from 'hooks/api'
 import { useTotalValue } from '../../../state/hooks'
 import CardValue from './CardValue'
+import { useState } from 'react';
 
 
 const StyledNotificationCard = styled(Card)`
