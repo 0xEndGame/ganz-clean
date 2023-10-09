6ad7d54653d8b381c876a569c9e5393e1c99cb65
Your mom <you@example.com>
2023-10-09 07:29:19 +0200

    remove x

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'WagmiFlip'
+            screenName: '#'
           }}
           options={{
             height: '300',
